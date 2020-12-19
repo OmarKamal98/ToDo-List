66ee385a069f38aff579b410ac77d8e17325ae90
OmarKamal98 <32268097+OmarKamal98@users.noreply.github.com>
2020-12-19 16:24:15 +0200

    ToDO-List

---
+++ app/src/main/java/com/omar98k/todolist/Lists.java
@@ -0,0 +1,21 @@
+package com.omar98k.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lists);
+    }
+
+    public void back(View view) {
+    }
+
+    public void OnClickCreateNewList(View view) {
+    }
+}
