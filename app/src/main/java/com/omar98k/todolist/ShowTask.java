7c03911891efb36522d098049d86ebd08fbcdcf0
OmarKamal98 <32268097+OmarKamal98@users.noreply.github.com>
2020-12-20 19:18:13 +0200

    ToDO-List

---
+++ app/src/main/java/com/omar98k/todolist/ShowTask.java
@@ -0,0 +1,24 @@
+package com.omar98k.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ShowTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_task);
+    }
+
+    public void backtask(View view) {
+    }
+
+    public void OnClickCreateNewtask(View view) {
+    }
+
+    public void delete(View view) {
+    }
+}
