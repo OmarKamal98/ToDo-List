9c0e6949ed22a24ba9abc3a7ae8ab4b275cf56eb
OmarKamal98 <oma98kam@gmail.com>
2020-12-25 14:31:53 +0200

    ToDO-List1111

---
+++ app/src/main/java/com/omar98k/todolist/AddTask.java
@@ -0,0 +1,21 @@
+package com.omar98k.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+
+    public void backFromAddNewActivity(View view) {
+    }
+
+    public void OnClickAdd(View view) {
+    }
+}
